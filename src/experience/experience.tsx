b9328d2934f70ab82561b271424820fa71c94598
xatxay <xatxaysmk@gmail.com>
2024-01-13 14:00:42 -0600

    edited experience

--- src/experience/experience.tsx
+++ src/experience/experience.tsx
@@ -5,34 +5,48 @@ const Experience = () => {
     <ExperienceContainer id="experience">
       <TextContainer>
         <h1>Experience :</h1>
-        <h2>Coder</h2>
-        <h3>From July 2023 - Now</h3>
+        <h2>Huma News Dashboard</h2>
+        <h3>From Dec 2023 - Now</h3>
         <span>
-          A fullstack developer specialized in javascript/typescript, react,
-          nodejs, postgresql, express and more.{" "}
+          -Created real-time data ingestion micro-services, supplementing a
+          variety of candle data processing from Binance <br />
+          -Integrated Postgres database to assist clients news headline
+          dashboard <br />
+          -Set up a REST api <br />
+          -Created an entire fullstack web app <br />
+          -Tech involved: Javascript/Typescript, React, Nodejs, Postgresql,
+          Express, Lightweight chart, AWS, Binance,...
         </span>
         <h2>INVESTOR/TRADER</h2>
         <h3>From 2021 - Now</h3>
         <span>
-          I mainly invest in cryptocurrency, and trade narratives. I use AI and
-          twitter feed to help me gauge the market sentiment. I've also built a
-          personal AI news trading terminal and tool (stink biddorrr) to assist
-          my trading
+          -I mainly invest in cryptocurrency, and trade narratives. <br />
+          -I use AI and twitter feed to help me gauge the market sentiment and
+          make trading decision based on the sentiment <br />
+          -I've also built a personal AI news trading terminal and tool (stink
+          biddorrr) to assist my trading <br />
+          -News trading terminal gives me the speed advantage that allow me to
+          enter position before majority of the people <br />I built Stink
+          -Biddorrr to automate placing bids and rebalance my orders every 24
+          hours
         </span>
         <h2>SNEAKERSSOLES</h2>
         <h3>From 2017 - Now</h3>
         <span>
-          I optimized business calendaring to appropriate sales with sneaker
-          releases and also provided an intersection of commerce between
-          consumers and latest fashion trends.
+          -Optimized business calendaring to appropriate sales with sneaker
+          releases <br />
+          -Increased sales by an average of 15% month to month by accommodating
+          sale promotions with calendar sneaker releases <br />
+          -Provided an intersection of commerce between consumers and latest
+          fashion trends. <br />
         </span>
-        <h2>News Dashboard</h2>
+        {/* <h2>News Dashboard</h2>
         <h3>From Dec 2023 - Now</h3>
         <span>
           Building a fullstack web app for cryptocurrency news headline events
           with live price data chart from scratch. It's a tool to help traders
           do research and stay inform on cryptocurrency market.
-        </span>
+        </span> */}
       </TextContainer>
     </ExperienceContainer>
   );
